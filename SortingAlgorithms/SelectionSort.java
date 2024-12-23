ead76b558b1d9bda17ee3369a6667d07515115c9
munirsiddiqui54 <munir22it@student.mes.ac.in>
2024-12-23 11:53:15 +0530

    modified Selection sort

--- SortingAlgorithms/SelectionSort.java
+++ SortingAlgorithms/SelectionSort.java
@@ -4,11 +4,10 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr={2,4,1,3,5};
+        int[] arr={1,-4};
         System.out.println(Arrays.toString(arr));
         selectionSort(arr);
         System.out.println(Arrays.toString(arr));
-
     }
     public static void swap(int[] nums,  int i,int j){
         int temp=nums[i];
@@ -25,7 +24,18 @@ public class SelectionSort {
         return max;
     }
     public static void selectionSort(int[] nums){
-        // TO BE IMPLEMENTED
+        int N=nums.length;
+
+        //select one element and place it on the correct position
+        for(int i=0;i<N;i++){
+
+            // get the index of maximum number
+            int index=getMaxIndex(nums,0,N-i-1);
+
+            //swap it with correct index
+            swap(nums,index,N-i-1);
+
+        }
     }
 
 }
